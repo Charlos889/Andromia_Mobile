528a2c3ed8548f280be942ffb36b5dc63de06fdb
Charlos889 <1625258@cstj.qc.ca>
2018-11-16 14:22:33 -0500

    scanner

---
+++ App/app/src/main/java/ca/qc/cstj/andromia/Activities/ScanActivity.kt
@@ -0,0 +1,40 @@
+package ca.qc.cstj.andromia.Activities
+
+import android.app.Activity
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import ca.qc.cstj.andromia.R
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
+import kotlinx.android.synthetic.main.activity_scan.*
+
+class ScanActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan)
+
+        btnScan.setOnClickListener {
+            val scanner = IntentIntegrator(this)
+
+            scanner.initiateScan()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(resultCode == Activity.RESULT_OK) {
+            val result  = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+            if(result != null) {
+                if(result.contents == null) {
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "Scanned: "+ result.contents, Toast.LENGTH_LONG).show()
+                }
+            } else {
+                super.onActivityResult(requestCode, resultCode, data)
+            }
+        }
+    }
+}
--- App/app/src/main/java/ca/qc/cstj/andromia/MainActivity.kt
+++ App/app/src/main/java/ca/qc/cstj/andromia/MainActivity.kt
@@ -1,12 +1,20 @@
 package ca.qc.cstj.andromia
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ca.qc.cstj.andromia.Activities.ScanActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnScanActivity.setOnClickListener {
+            val intent = Intent(this, ScanActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

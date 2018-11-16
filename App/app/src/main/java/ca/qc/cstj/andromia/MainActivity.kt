04482295882b886f5047cf36fb5a28a0ee05cb79
Charles Ménard <1625258@cstj.qc.ca>
2018-11-16 13:29:26 -0500

    Création project Android Studio

---
+++ App/app/src/main/java/ca/qc/cstj/andromia/MainActivity.kt
@@ -0,0 +1,12 @@
+package ca.qc.cstj.andromia
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
